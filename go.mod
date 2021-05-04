5d2f4f581d2dae82236e330c7dc1b56e8ca42363
cerebrium <nicholas.m.shankland@gmail.com>
2021-05-04 15:06:34 -0700

    added a sorting route

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module backend
 go 1.15
 
 require (
-	github.com/gofiber/fiber/v2 v2.5.0
-	go.mongodb.org/mongo-driver v1.4.6
+	github.com/gofiber/fiber/v2 v2.8.0
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.5.1
 )
--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -17,17 +19,150 @@ import (
 
 // structures
 type Color struct {
-	_id string `json:"id,omitempty"`
+	Id string `json:"_id,omitempty"`
 
 	Color string `json:"color,omitempty"`
 
 	Name string `json:"name,omitempty"`
 }
 
+// structure for the color object thats sortable
+type ColorObject struct {
+	Id string
+
+	Color string
+
+	Name string
+
+	Blue int16
+
+	Red int16
+
+	Green int16
+
+	Chroma float64
+
+	Sat float64
+
+	Val float64
+
+	Luma float64
+
+	Hue float64
+}
+
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+// array maker helper
+func createColor(color Color) ColorObject {
+	// make the object to return
+	m := new(ColorObject)
+
+	// take the color and generate red, blue and green
+	r, err := strconv.ParseInt(color.Color[1:3], 16, 64)
+	g, err := strconv.ParseInt(color.Color[3:5], 16, 64)
+	b, err := strconv.ParseInt(color.Color[5:7], 16, 64)
+
+	// check for error
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// divide and convert type
+	r2 := float64(r) / float64(255)
+	g2 := float64(g) / float64(255)
+	b2 := float64(b) / float64(255)
+
+	// instantiate values for min and max
+	min := float64(1)
+	max := float64(0)
+
+	// get max and min for the chroma
+	colorArray := []float64{r2, g2, b2}
+
+	// loop through array and determine min an max
+	for _, color := range colorArray {
+		if color > max {
+			max = color
+		}
+
+		if color < min {
+			min = color
+		}
+	}
+
+	// HSV values of hex color
+	chr := max - min
+	hue := float64(0)
+	val := max
+	sat := float64(0)
+
+	if val > 0 {
+		// Calculate Saturation only if Value isn't 0
+		sat = chr / val
+		if sat > 0 {
+			if r2 == max {
+				hue = 60 * (((g2 - min) - (b2 - min)) / chr)
+				if hue < 0 {
+					hue += 360
+				}
+			} else if g2 == max {
+				hue = 120 + 60*(((b2-min)-(r2-min))/chr)
+			} else if b2 == max {
+				hue = 240 + 60*(((r2-min)-(g2-min))/chr)
+			}
+		}
+	}
+
+	// set the values to the map to return
+	m.Id = color.Id
+	m.Name = color.Name
+	m.Color = color.Color
+	m.Blue = int16(b)
+	m.Red = int16(r)
+	m.Green = int16(g)
+	m.Chroma = chr
+	m.Hue = hue
+	m.Luma = 0.3*r2 + 0.59*g2 + 0.11*b2
+	m.Sat = sat
+	m.Val = val
+
+	return *m
+}
+
+// function to sort colors
+func createSortableArray(array []Color) []ColorObject {
+	// instantiate the array to return
+	var finalArray []ColorObject
+
+	// loop through the array and append each new color
+	for _, color := range array {
+		finalArray = append(finalArray, createColor(color))
+	}
+
+	// return the array
+	return finalArray
+}
+
 // function for accessing the database
 func GetMongoDbConnection() (*mongo.Client, error) {
 
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb+srv://admin:password1A@cyrptocluster.lmp7s.mongodb.net/sample_airbnb?retryWrites=true&w=majority"))
+	// grab the url for the database
+	dotenv := goDotEnvVariable("MONGO_STRING")
+
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(dotenv))
 
 	if err != nil {
 		log.Fatal(err)
@@ -92,6 +227,10 @@ func findOneColor(ColorId string) bool {
 }
 
 func main() {
+	// load the env file
+	godotenv.Load()
+
+	// instantiate instance of fiber
 	app := fiber.New()
 
 	// allow for cors
@@ -129,9 +268,73 @@ func main() {
 			return c.Status(404).SendString("not Found")
 		}
 
-		// send in json format the results
-		json, _ := json.Marshal(results)
+		// turn the data into json
+		json, err := json.Marshal(results)
+
+		// handle errors
+		if err != nil {
+			return c.Status(500).Send([]byte(err.Error()))
+		}
+
+		// send the data
 		return c.Send(json)
+	})
+
+	app.Get("/colors", func(c *fiber.Ctx) error {
+		// connect to the database
+		collection, err := getMongoDbCollection("hhtest", "colors")
+		if err != nil {
+			// error in connection return error
+			return c.Status(500).Send([]byte(err.Error()))
+		}
+
+		// filter
+		var filter bson.M = bson.M{}
+
+		// make the results be in the correct format
+		var results []bson.M
+
+		// actually make the request using the cursor
+		cur, err := collection.Find(context.Background(), filter, options.Find())
+		defer cur.Close(context.Background())
+
+		// handle errors
+		if err != nil {
+			return c.Status(500).Send([]byte(err.Error()))
+		}
+
+		// grab all of the results from the quesry
+		cur.All(context.Background(), &results)
+
+		// handle errors
+		if results == nil {
+			return c.Status(404).SendString("not Found")
+		}
+
+		// send in json format the results
+		jsonData, err := json.Marshal(results)
+
+		// handle errors
+		if err != nil {
+			return c.Status(500).Send([]byte(err.Error()))
+		}
+
+		// instantiate type array of Color
+		var data []Color
+
+		// unmarshal the []byte into usable format
+		json.Unmarshal(jsonData, &data)
+
+		// remarshal back to []byte
+		jsonFinal, err := json.Marshal(createSortableArray(data))
+
+		// handle errors
+		if err != nil {
+			return c.Status(500).Send([]byte(err.Error()))
+		}
+
+		// send the converted data
+		return c.Send(jsonFinal)
 	})
 
 	// get color by id
